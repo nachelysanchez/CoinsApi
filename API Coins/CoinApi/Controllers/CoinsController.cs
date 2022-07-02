093ad44616ebff38c1ee3f771e5961fb1559a405
nachelysanchez <nashely-5@hotmail.com>
2022-07-01 23:49:42 -0400

    Agregando POST

--- API Coins/CoinApi/Controllers/CoinsController.cs
+++ API Coins/CoinApi/Controllers/CoinsController.cs
@@ -7,29 +7,52 @@ namespace CoinApi.Controllers;
 [Route("[controller]")]
 public class CoinsController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private Contexto _companyContext;
+
+    public CoinsController(Contexto companyContext)
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        _companyContext = companyContext;
+    }
+    
 
-     private Contexto _companyContext;
-
-        public CoinsController(Contexto companyContext)
-        {
-            _companyContext = companyContext;
-        }
-    /*private readonly ILogger<CoinsController> _logger;
-    private readonly Contexto contexto;
-
-    public CoinsController(ILogger<CoinsController> logger, Contexto _contexto)
-    {
-        _logger = logger;
-        _contexto = contexto;
-    }*/
-
-    [HttpGet(Name = "GetCoins")]
+    /*[HttpGet]
     public IEnumerable<Coins> Get()
     {
         return _companyContext.Coins;
+    }*/
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Coins>>> GetCoins(){
+        var lista = await _companyContext.Coins.ToListAsync();
+        return lista;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Coins>> PostCoin(Coins coin){
+        try{
+            if (coin.MonedaId >= 0){
+                if(CoinExiste(coin.MonedaId)){
+                    _companyContext.Entry(coin).State = EntityState.Modified;
+                    await _companyContext.SaveChangesAsync();
+                    return Ok();
+                }
+                else{
+                    _companyContext.Coins.Add(coin);
+                    await _companyContext.SaveChangesAsync();
+                    return CreatedAtAction("GetCoins", new {id = coin.MonedaId}, coin);
+                }
+            }
+            else{
+                return CreatedAtAction("PostAsync", coin);
+            }
+        }
+        catch(Exception){
+            throw;
+        }
+    }
+
+    private bool CoinExiste(int id){
+        var existe = _companyContext.Coins.Any(x => x.MonedaId == id);
+        return existe;
     }
 }
